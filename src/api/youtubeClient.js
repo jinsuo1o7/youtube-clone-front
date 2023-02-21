f181513cee7ee8d6200e5365f87e2f88c47f0388
Jinsuo <cil04110@gmail.com>
2023-02-21 23:44:06 +0900

    ifram https

--- src/api/youtubeClient.js
+++ src/api/youtubeClient.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export default class YoutubeClient {
   constructor() {
     this.httpClient = axios.create({
-      baseURL: "https://youtube.googleapis.com/youtube/v3",
+      baseURL: "https://www.youtube.googleapis.com/youtube/v3",
       params: { key: process.env.REACT_APP_YOUTUBE_API_KEY },
     });
   }
